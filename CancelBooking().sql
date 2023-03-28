f32f3d4941414f2e9bf84ae396ba2ece87154db2
raknarNoob <103694321+raknarNoob@users.noreply.github.com>
2023-03-28 11:56:10 +0100

    Update CancelBooking().sql

--- CancelBooking().sql
+++ CancelBooking().sql
@@ -1,7 +1,6 @@
 DROP PROCEDURE IF EXISTS CancelBooking; 
-DELIMITER //
+
 CREATE PROCEDURE CancelBooking(IN BookingID INT) 
 BEGIN 
 DELETE FROM bookings WHERE BookingID = BookingID; 
-SELECT CONCAT("Booking ", BookingID, " cancelled") AS "Confirmation"; 
-END$$ DELIMITER ; 
+END 
