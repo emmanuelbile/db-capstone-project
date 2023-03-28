ff407986eb32117b5aed14e7213de7f004bfad65
raknarNoob <103694321+raknarNoob@users.noreply.github.com>
2023-03-28 12:03:34 +0100

    Update AddBooking().sql

--- AddBooking().sql
+++ AddBooking().sql
@@ -1,7 +1,6 @@
 DROP PROCEDURE IF EXISTS AddBooking; 
-DELIMITER //
 CREATE PROCEDURE AddBooking(IN BookingID INT, IN CustomerID INT, IN TableNumber INT, IN BookingDate DATE)
 BEGIN 
-INSERT INTO bookings (BookingID, CustomerID, TableNumber, date) VALUES (BookingID, CustomerID, TableNumber, BookingDate); 
-SELECT "New booking added" AS "Confirmation";
-END$$ DELIMITER ; 
+INSERT INTO Bookings (BookingID, CustomerID, TableNumber, date) VALUES (BookingID, CustomerID, TableNumber, BookingDate); 
+
+END
